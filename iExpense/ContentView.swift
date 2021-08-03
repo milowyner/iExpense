4d3298803c14eb33e68c791deddc4f0626e9a67d
Milo Wyner <milowyner@gmail.com>
2021-08-03 15:24:06 -0700

    Add list with button to add placeholder expense items, using observable object so later views can access the same data

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,41 @@
 
 import SwiftUI
 
+struct ExpenseItem: Identifiable {
+    let name: String
+    let type: String
+    let amount: Int
+    
+    let id = UUID()
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .navigationBarItems(trailing: Button(action: {
+                let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                expenses.items.append(expense)
+            }, label: {
+                Image(systemName: "plus")
+            }))
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
